bef28c4dec2254cf4a8bb6d65a6d5b64a87e8c39
Dexter Yang <dexter.yang@qq.com>
2019-10-08 18:16:48 +0800

    init

--- app/src/main/java/net/gridtech/display/tea_hori/MainActivity.kt
+++ app/src/main/java/net/gridtech/display/tea_hori/MainActivity.kt
@@ -4,18 +4,51 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_main.*
 import net.gridtech.display.core.CoreService
 import net.gridtech.display.core.CoreServiceBinder
+import net.gridtech.display.tea_hori.ui.UiManager
 
-class MainActivity : AppCompatActivity(),ServiceConnection  {
+
+class MainActivity : AppCompatActivity(), ServiceConnection {
     var binder: CoreServiceBinder? = null
+    override fun onResume() {
+        super.onResume()
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         setContentView(R.layout.activity_main)
+
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+
+
+        businessView.isLongClickable = true
+        businessView.setOnLongClickListener {
+            println("====")
+            UiManager.longClickSubject.onNext(it)
+            true
+        }
+
+
+
         bindService(
             Intent(this, CoreService::class.java),
             this,
@@ -26,9 +59,11 @@ class MainActivity : AppCompatActivity(),ServiceConnection  {
             startActivity(Intent("net.gridtech.display.core.CoreActivity"))
         }
     }
+
     override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
         binder = service as CoreServiceBinder
     }
+
     override fun onServiceDisconnected(name: ComponentName?) {
         binder = null
     }
--- core/src/main/java/net/gridtech/display/core/CoreActivity.kt
+++ core/src/main/java/net/gridtech/display/core/CoreActivity.kt
@@ -5,20 +5,36 @@ import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
 import android.graphics.Color
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.IBinder
+import android.support.v7.app.AppCompatActivity
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_core.*
 import net.gridtech.core.Bootstrap
 import net.gridtech.core.util.hostInfoPublisher
 
-class CoreActivity : AppCompatActivity(),ServiceConnection {
+
+class CoreActivity : AppCompatActivity(), ServiceConnection {
     var binder: CoreServiceBinder? = null
-    val handler=Handler()
+    val handler = Handler()
+    override fun onResume() {
+        super.onResume()
+        window.decorView.systemUiVisibility  = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_core)
+        window.decorView.systemUiVisibility  = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+
+
         bindService(
             Intent(this, CoreService::class.java),
             this,
@@ -34,15 +50,19 @@ class CoreActivity : AppCompatActivity(),ServiceConnection {
                 )
             )
         }
+        back.setOnClickListener {
+            finish()
+        }
     }
+
     override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
         binder = service as CoreServiceBinder
         showHostInfo()
         binder?.bootstrap?.connectionObservable()?.subscribe {
             handler.post {
-                if(it){
+                if (it) {
                     connectionIcon.setColorFilter(Color.GREEN)
-                }else{
+                } else {
                     connectionIcon.setColorFilter(Color.RED)
                 }
             }
@@ -63,5 +83,38 @@ class CoreActivity : AppCompatActivity(),ServiceConnection {
         nodeId.setText(binder?.hostInfo?.nodeId)
         nodeSecret.setText(binder?.hostInfo?.nodeSecret)
         parentAccess.setText(binder?.hostInfo?.parentEntryPoint)
+    }
+
+    override fun dispatchTouchEvent(me: MotionEvent): Boolean {
+        if (me.action == MotionEvent.ACTION_DOWN) {  //把操作放在用户点击的时候
+            val v = currentFocus      //得到当前页面的焦点,ps:有输入框的页面焦点一般会被输入框占据
+            if (isShouldHideKeyboard(v, me)) { //判断用户点击的是否是输入框以外的区域
+                hideKeyboard(v!!.windowToken)   //收起键盘
+            }
+        }
+        return super.dispatchTouchEvent(me)
+    }
+
+    private fun isShouldHideKeyboard(v: View?, event: MotionEvent): Boolean {
+        if (v != null && v is EditText) {  //判断得到的焦点控件是否包含EditText
+            val l = intArrayOf(0, 0)
+            v.getLocationInWindow(l)
+            val left = l[0]
+            //得到输入框在屏幕中上下左右的位置
+            val top = l[1]
+            val bottom = top + v.height
+            val right = left + v.width
+            return !(event.x > left && event.x < right
+                    && event.y > top && event.y < bottom)
+        }
+        // 如果焦点不是EditText则忽略
+        return false
+    }
+
+    private fun hideKeyboard(token: IBinder?) {
+        if (token != null) {
+            val im = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            im.hideSoftInputFromWindow(token, InputMethodManager.HIDE_NOT_ALWAYS)
+        }
     }
 }
