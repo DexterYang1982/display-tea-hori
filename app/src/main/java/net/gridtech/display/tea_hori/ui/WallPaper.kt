0746bddf60dce255fd02fec72b6d17eafb2bd277
Dexter Yang <dexter.yang@qq.com>
2019-11-09 17:11:07 +0800

    init

--- app/src/main/java/net/gridtech/display/tea_hori/ui/WallPaper.kt
+++ app/src/main/java/net/gridtech/display/tea_hori/ui/WallPaper.kt
@@ -1,6 +1,7 @@
 package net.gridtech.display.tea_hori.ui
 
 import android.content.Intent
+import kotlinx.android.synthetic.main.activity_wall_paper.*
 import net.gridtech.display.core.view.BaseView
 import net.gridtech.display.tea_hori.R
 
@@ -9,5 +10,9 @@ class WallPaper : BaseView() {
     override fun shouldBindService(): Boolean = true
     override fun setLayout() {
         setContentView(R.layout.activity_wall_paper)
+    }
+
+    override fun initLogic() {
+        imageView.setImageResource(R.drawable.wallpaper_1)
     }
 }
